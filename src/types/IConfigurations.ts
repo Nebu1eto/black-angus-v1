181b508fc54a3728ef3b014804435e77ca0ba2e5
Haze Lee <hazelee@realignist.me>
2019-10-03 11:18:00 +0900

    add: configuration interface

---
+++ src/types/IConfigurations.ts
@@ -0,0 +1,5 @@
+interface IConfigurations {
+  DEBUG_HISTORY_TO_CHANNEL: boolean         // true
+  DEBUG_ERROR_TO_CHANNEL: boolean           // true
+  DEBUG_HISTORY_OR_ERROR_CHANNEL: string    // "log_channel"
+}

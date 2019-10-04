a18baa81ad7117e953e21ad6d707722efcd7d59d
Haze Lee <hazelee@realignist.me>
2019-10-05 01:11:38 +0900

    chore: add pm2 ecosystem file

---
+++ pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps : [{
+    name: 'Black Angus',
+    script: 'ts-node',
+    args: 'src/index.ts',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      CONFIG_FILE: './env/config.json'
+    }
+  }]
+};
